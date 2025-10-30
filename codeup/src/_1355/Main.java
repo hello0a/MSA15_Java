bea23d56b3e839c8ec6a330e4f15eddb8c4c8280
hello0a <aya70713@gmail.com>
2025-10-30 15:17:39 +0900

    ~9일차

--- codeup/src/_1355/Main.java
+++ codeup/src/_1355/Main.java
@@ -14,10 +14,12 @@ public class Main {
 		//			 **
 		//			 ***
 		for (int i = 0; i <= n; i++) {
+			
+			for (int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}
+			
 			for (int j = n; j > i; j--) {
-				int temp = i;
-				i = j;
-				j = i;
 				System.out.print("*");
 			}
 			System.out.println();
